a459f2c34d9e1504cf38f174098cc6762f15054f
Alif Triadi Agung W <aliftriadi.gaming@gmail.com>
2022-12-12 11:45:15 +0700

    Converting SliverAppBar to Widget

--- lib/views/screens/home_page.dart
+++ lib/views/screens/home_page.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:pedo/constant/assets_path.dart';
 import 'package:pedo/constant/themes.dart';
 import 'package:pedo/core/providers/article_provider.dart';
-import 'package:pedo/core/providers/auth_provider.dart';
 import 'package:pedo/views/screens/article/article_page.dart';
-import 'package:pedo/views/screens/more_page.dart';
 import 'package:pedo/views/widgets/article_card.dart';
 import 'package:pedo/views/widgets/badge.dart';
+import 'package:pedo/views/widgets/custom_appbar.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -16,7 +15,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    AuthProvider authProvider = Provider.of<AuthProvider>(context);
     ArticleProvider articleProvider = Provider.of<ArticleProvider>(context);
 
     Widget articleHeader() {
@@ -118,73 +116,7 @@ class HomePage extends StatelessWidget {
               parent: BouncingScrollPhysics(),
             ),
             slivers: [
-              SliverAppBar(
-                toolbarHeight: 60,
-                backgroundColor: background,
-                floating: true,
-                elevation: 0,
-                title: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      InkWell(
-                        onTap: () => Navigator.pushNamed(
-                          context,
-                          MorePage.route,
-                        ),
-                        child: Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(100),
-                              child: authProvider.getUser?.image != null
-                                  ? Image.network(
-                                      authProvider.getUser!.image,
-                                      width: 40,
-                                      height: 40,
-                                      fit: BoxFit.cover,
-                                    )
-                                  : const Text('No Image'),
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Halo,',
-                                  style: subtitleTextStyle.copyWith(
-                                    fontWeight: semibold,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                Text(
-                                  "${authProvider.getUser!.name.split(RegExp(r'\s+'))[0]} 👋",
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                  style: primaryTextStyle.copyWith(
-                                    fontWeight: semibold,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.notifications,
-                          color: colorSubtitle,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                leading: null,
-              ),
+              CustomAppBar(),
               SliverToBoxAdapter(
                 child: ListView(
                   physics: const NeverScrollableScrollPhysics(),
---
+++ lib/views/widgets/custom_appbar.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:pedo/constant/themes.dart';
+import 'package:pedo/core/providers/auth_provider.dart';
+import 'package:pedo/views/screens/more_page.dart';
+import 'package:provider/provider.dart';
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+
+    return SliverAppBar(
+      toolbarHeight: 60,
+      backgroundColor: background,
+      floating: true,
+      elevation: 0,
+      title: Padding(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            InkWell(
+              onTap: () => Navigator.pushNamed(
+                context,
+                MorePage.route,
+              ),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: authProvider.getUser?.image != null
+                        ? Image.network(
+                            authProvider.getUser!.image,
+                            width: 40,
+                            height: 40,
+                            fit: BoxFit.cover,
+                          )
+                        : const Text('No Image'),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Halo,',
+                        style: subtitleTextStyle.copyWith(
+                          fontWeight: semibold,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        "${authProvider.getUser!.name.split(RegExp(r'\s+'))[0]} 👋",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: primaryTextStyle.copyWith(
+                          fontWeight: semibold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications,
+                color: colorSubtitle,
+              ),
+            ),
+          ],
+        ),
+      ),
+      leading: null,
+    );
+  }
+}
