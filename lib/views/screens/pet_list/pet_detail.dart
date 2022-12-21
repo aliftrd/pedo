6462e1c3ca4a341d279a8dbd726284a6ba0de8eb
wtegar <wahyutegarr1@gmail.com>
2022-12-21 13:27:12 +0700

    slicing

--- lib/views/screens/pet_list/pet_detail.dart
+++ lib/views/screens/pet_list/pet_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:pedo/constant/themes.dart';
 import 'package:pedo/constant/assets_path.dart';
+import 'package:pedo/main.dart';
 import 'package:pedo/views/widgets/badge.dart';
 import 'package:pedo/views/screens/pet_list/pet_list.dart';
 
@@ -57,26 +58,226 @@ class _petDetailState extends State<petDetail> {
               fontSize: 18,
               fontWeight: semibold,
             ),
-          )
+          ),
         ],
       );
     }
 
-    Widget DetailPetMid() {
-      return Row(
-        children: [
-          Row(
-            children: [
-              ClipRRect(
-                child: Image.asset(
-                  'assets/images/kucing.png',
-                  width: 40,
-                  height: 40,
+    Widget detailPetMid() {
+      return Container(
+        margin: const EdgeInsets.only(top: 25),
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.asset(
+                    'assets/images/kucing.png',
+                    width: 40,
+                    height: 40,
+                  ),
                 ),
-              )
-            ],
-          )
-        ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 7),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Mahamatra',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 14,
+                          fontWeight: regular,
+                        ),
+                      ),
+                      Text(
+                        'Jember',
+                        style: subtitleTextStyle.copyWith(
+                          fontSize: 11,
+                          fontWeight: regular,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 32,
+                  height: 32,
+                  decoration: BoxDecoration(
+                    color: colorPrimary,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Icon(
+                    size: 14,
+                    Icons.message,
+                    color: colorLight,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 32,
+                  height: 32,
+                  decoration: BoxDecoration(
+                    color: colorSuccess,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Icon(
+                    size: 14,
+                    Icons.location_on,
+                    color: colorLight,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget detailHewan() {
+      return Container(
+        padding: EdgeInsets.only(
+          top: 25,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Jenis',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 2,
+                    ),
+                    Text(
+                      'Ras',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 2,
+                    ),
+                    Text(
+                      'Gender',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 2,
+                    ),
+                    Text(
+                      'Warna Primer',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 2,
+                    ),
+                    Text(
+                      'Warna Primer',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 2,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 120,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Kucing',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    Text(
+                      'Anggora',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    Text(
+                      'Laki-Laki',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    Text(
+                      'Kuning',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                    Text(
+                      'Putih',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: regular,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 10),
+            Column(
+              children: [
+                Text(
+                  'Deskripsi Hewan',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: semibold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 5),
+            Column(
+              children: [
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa tincidunt arcu pulvinar etiam nibh risus. Venenatis mauris adipiscing in facilisis sit sed id a. Diam urna, proin faucibus aliquet lectus. Viverra amet justo bibendum tincidunt leo. Urna magna ac eget sodales purus est integer tellus eget. Tincidunt urna amet sed bibendum mattis nulla velit lacus. Neque, pretium, ligula varius imperdiet et et vivamus. Quam eget non consequat aliquam tempus suspendisse turpis mi tincidunt. Ante et nunc faucibus sed tellus malesuada fringilla elit. At id quis non suspendisse. Suspendisse luctus vel iaculis rhoncus, eu. In fringilla ut tempor, mi diam sed non vestibulum. Pellentesque sed vel nullam et. Blandit eu dui est sagittis, amet, nunc aliquet. Ut augue sodales quis at vitae eget mauris. Et mauris venenatis sed sagittis, et eu. Condimentum urna habitasse pellentesque hendrerit consequat viverra ut. ',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 14,
+                    fontWeight: regular,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       );
     }
 
@@ -118,7 +319,8 @@ class _petDetailState extends State<petDetail> {
                     padding: EdgeInsets.symmetric(horizontal: defaultMargin),
                     child: detailPet(),
                   ),
-                  DetailPetMid(),
+                  detailPetMid(),
+                  detailHewan(),
                 ],
               ),
             ),
